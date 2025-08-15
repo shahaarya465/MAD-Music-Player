f93049cbf50c280b37de58a20153f90cfa06e268
KavyaJP <kavya31052005@gmail.com>
2025-08-15 16:45:37 +0530

    Removed redundant code

--- lib/music_player_screen.dart
+++ lib/music_player_screen.dart
@@ -185,13 +185,6 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
     );
   }
 
-  String _formatDuration(Duration duration) {
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
-    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
-    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
-    return "$twoDigitMinutes:$twoDigitSeconds";
-  }
-
   @override
   Widget build(BuildContext context) {
     // NEW: Get the ThemeManager from the provider
